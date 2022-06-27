bdb52ca3933d955409f3806c963c6361ed61c39f
jvkass <jvcnmata@gmail.com>
2022-06-27 12:55:32 -0300

    Criação DTO do create e implementação da rota de busca por id e exclusão por id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -11,11 +11,21 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(
     @Body('title') title: string,
     @Body('description') description: string,
   ): Task {
-    return this.tasksService.createTask(title, description);
+    return this.tasksService.createTask({ title, description });
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void{
+    return this.tasksService.deleteTask(id);
   }
 }
