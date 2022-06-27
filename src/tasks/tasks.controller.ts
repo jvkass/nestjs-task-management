ba3713b4d81633c486459003d85c64f570493210
jvkass <jvcnmata@gmail.com>
2022-06-27 13:47:56 -0300

    service e controller referente a querys no retorno das tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -15,8 +17,14 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+
+    if (Object.keys(filterDto).length){
+      return this.tasksService.getTasksWithFilters(filterDto)
+    }else {
+      return this.tasksService.getAllTasks();
+    }
+
   }
 
   @Get('/:id')
