4206374d3c1a83f53acfc0b597143862d66489ef
jvkass <jvcnmata@gmail.com>
2022-07-04 11:09:39 -0300

    Finalizando persistencia no postgres

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,17 +1,17 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./task.model";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    status: TaskStatus;
+  @Column()
+  status: TaskStatus;
 }
